0ebc204cfb825ae2924b7d17412c9ab5c1407884
Tomás S. Grigera <tgrigera@iflysib.unlp.edu.ar>
2021-02-01 21:51:06 -0300

    Texified model_desc

--- model_desc/gen_README.sh
+++ model_desc/gen_README.sh
@@ -1,6 +1,6 @@
 # for now I am using TeXify on GitHub
-python3 -m readme2tex  --svgdir model_desc/svgs/ --output README.md README.tex.md
-mv model_desc/svgs .
-rmdir model_desc
+python3 -m readme2tex  --output README.md README.tex.md
+rm -rf ../svgs
+mv svgs ..
 
 
